04a16fb70344f89518d2d4401c6a945993d4f82a
Tanmay09s <tanmayshahapure@gmail.com>
2026-07-12 19:15:46 +0530

    backend changes

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -56,7 +56,6 @@ export async function signup(req, res) {
       maxAge: 7 * 24 * 60 * 60 * 1000,
       httpOnly: true, // prevent XSS attacks,
       sameSite: "strict", // prevent CSRF attacks
-      secure: process.env.NODE_ENV === "production",
     });
 
     res.status(201).json({ success: true, user: newUser });
@@ -88,7 +87,6 @@ export async function login(req, res) {
       maxAge: 7 * 24 * 60 * 60 * 1000,
       httpOnly: true, // prevent XSS attacks,
       sameSite: "strict", // prevent CSRF attacks
-      secure: process.env.NODE_ENV === "production",
     });
 
     res.status(200).json({ success: true, user });
--- backend/src/server.js
+++ backend/src/server.js
@@ -21,13 +21,6 @@ app.use(
   })
 );
 
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL,
-    credentials: true,
-  })
-);
-
 app.use(express.json());
 app.use(cookieParser());
 
